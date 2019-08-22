020dba8e2e67440e2e32a55377fb8b5d8fb6b879
Chris Eidhof <chris@eidhof.nl>
2019-08-22 12:58:49 +0200

    Flow layout

--- FlowLayoutST/ContentView.swift
+++ FlowLayoutST/ContentView.swift
@@ -8,34 +8,104 @@
 
 import SwiftUI
 
+struct FlowLayout {
+    let spacing: UIOffset
+    let containerSize: CGSize
+    
+    init(containerSize: CGSize, spacing: UIOffset = UIOffset(horizontal: 10, vertical: 10)) {
+        self.spacing = spacing
+        self.containerSize = containerSize
+    }
+    
+    var currentX = 0 as CGFloat
+    var currentY = 0 as CGFloat
+    var lineHeight = 0 as CGFloat
+    
+    mutating func add(element size: CGSize) -> CGRect {
+        if currentX + size.width > containerSize.width {
+            currentX = 0
+            currentY += lineHeight + spacing.vertical
+            lineHeight = 0
+        }
+        defer {
+            lineHeight = max(lineHeight, size.height)
+            currentX += size.width + spacing.horizontal
+        }
+        return CGRect(origin: CGPoint(x: currentX, y: currentY), size: size)
+    }
+    
+    var size: CGSize {
+        return CGSize(width: containerSize.width, height: currentY + lineHeight)
+    }
+}
+
+func flowLayout<Elements>(for elements: Elements, containerSize: CGSize, sizes: [Elements.Element.ID: CGSize]) -> [Elements.Element.ID: CGSize] where Elements: RandomAccessCollection, Elements.Element: Identifiable {
+    var state = FlowLayout(containerSize: containerSize)
+    var result: [Elements.Element.ID: CGSize] = [:]
+    for element in elements {
+        let rect = state.add(element: sizes[element.id] ?? .zero)
+        result[element.id] = CGSize(width: rect.origin.x, height: rect.origin.y)
+    }
+    return result
+}
+
+
+func singleLineLayout<Elements>(for elements: Elements, containerSize: CGSize, sizes: [Elements.Element.ID: CGSize]) -> [Elements.Element.ID: CGSize] where Elements: RandomAccessCollection, Elements.Element: Identifiable {
+    var result: [Elements.Element.ID: CGSize] = [:]
+    var offset = CGSize.zero
+    for element in elements {
+        result[element.id] = offset
+        let size = sizes[element.id] ?? CGSize.zero
+        offset.width += size.width + 10
+    }
+    return result
+}
+    
+
 struct CollectionView<Elements, Content>: View where Elements: RandomAccessCollection, Content: View, Elements.Element: Identifiable {
     var data: Elements
+    var layout: (Elements, CGSize, [Elements.Element.ID: CGSize]) -> [Elements.Element.ID: CGSize]
     var content: (Elements.Element) -> Content
-    var body: some View {
+    @State private var sizes: [Elements.Element.ID: CGSize] = [:]
+    
+    private func bodyHelper(containerSize: CGSize, offsets: [Elements.Element.ID: CGSize]) -> some View {
         ZStack(alignment: .topLeading) {
             ForEach(data) {
-                PropagateSize(content: self.content($0))
+                PropagateSize(content: self.content($0), id: $0.id)
+                    .offset(offsets[$0.id] ?? CGSize.zero)
+                    .animation(.default)
             }
+            Color.clear
+                .frame(width: containerSize.width, height: containerSize.height)
+                .fixedSize()
         }.onPreferenceChange(CollectionViewSizeKey.self) {
-            print($0)
+            self.sizes = $0
+            
+        }.background(Color.red)
+    }
+    
+    var body: some View {
+        GeometryReader { proxy in
+            self.bodyHelper(containerSize: proxy.size, offsets: self.layout(self.data, proxy.size, self.sizes))
         }
     }
 }
 
-struct CollectionViewSizeKey: PreferenceKey {
-    typealias Value = [CGSize]
+struct CollectionViewSizeKey<ID: Hashable>: PreferenceKey {
+    typealias Value = [ID: CGSize]
     
-    static var defaultValue: [CGSize] { [] }
-    static func reduce(value: inout [CGSize], nextValue: () -> [CGSize]) {
-        value.append(contentsOf: nextValue())
+    static var defaultValue: [ID: CGSize] { [:] }
+    static func reduce(value: inout [ID:CGSize], nextValue: () -> [ID:CGSize]) {
+        value.merge(nextValue(), uniquingKeysWith: { $1 })
     }
 }
 
-struct PropagateSize<V: View>: View {
+struct PropagateSize<V: View, ID: Hashable>: View {
     var content: V
+    var id: ID
     var body: some View {
         content.background(GeometryReader { proxy in
-            Color.clear.preference(key: CollectionViewSizeKey.self, value: [proxy.size])
+            Color.clear.preference(key: CollectionViewSizeKey<ID>.self, value: [self.id: proxy.size])
         })
     }
 }
@@ -48,12 +118,21 @@ extension String: Identifiable {
 
 struct ContentView: View {
     let strings: [String] = (1...10).map { "Item \($0) " + String(repeating: "x", count: Int.random(in: 0...10)) }
+    @State var dividerWidth: CGFloat = 100
     
     var body: some View {
-        CollectionView(data: strings) {
-            Text($0)
-                .padding(10)
-                .background(Color.gray)
+        VStack {
+            HStack {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: dividerWidth)
+                CollectionView(data: strings, layout: flowLayout) {
+                    Text($0)
+                        .padding(10)
+                        .background(Color.gray)
+                }
+            }
+            Slider(value: $dividerWidth, in: 0...500)
         }
     }
 }
@@ -63,3 +142,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
