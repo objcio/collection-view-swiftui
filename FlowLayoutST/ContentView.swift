cf63e8dc3aeb5411372974a10623f348b5df216d
Chris Eidhof <chris@eidhof.nl>
2019-08-22 12:11:34 +0200

    Flow Layout ep 1

--- FlowLayoutST/ContentView.swift
+++ FlowLayoutST/ContentView.swift
@@ -8,9 +8,53 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CollectionView<Elements, Content>: View where Elements: RandomAccessCollection, Content: View, Elements.Element: Identifiable {
+    var data: Elements
+    var content: (Elements.Element) -> Content
     var body: some View {
-        Text("Hello World")
+        ZStack(alignment: .topLeading) {
+            ForEach(data) {
+                PropagateSize(content: self.content($0))
+            }
+        }.onPreferenceChange(CollectionViewSizeKey.self) {
+            print($0)
+        }
+    }
+}
+
+struct CollectionViewSizeKey: PreferenceKey {
+    typealias Value = [CGSize]
+    
+    static var defaultValue: [CGSize] { [] }
+    static func reduce(value: inout [CGSize], nextValue: () -> [CGSize]) {
+        value.append(contentsOf: nextValue())
+    }
+}
+
+struct PropagateSize<V: View>: View {
+    var content: V
+    var body: some View {
+        content.background(GeometryReader { proxy in
+            Color.clear.preference(key: CollectionViewSizeKey.self, value: [proxy.size])
+        })
+    }
+}
+
+// todo hack
+
+extension String: Identifiable {
+    public var id: String { self }
+}
+
+struct ContentView: View {
+    let strings: [String] = (1...10).map { "Item \($0) " + String(repeating: "x", count: Int.random(in: 0...10)) }
+    
+    var body: some View {
+        CollectionView(data: strings) {
+            Text($0)
+                .padding(10)
+                .background(Color.gray)
+        }
     }
 }
 
