d1dcca24382911f3fa1fd86ba54345daaaa125a2
Chris Eidhof <chris@eidhof.nl>
2019-08-23 13:04:56 +0200

    Justified flow layout

--- FlowLayoutST/ContentView.swift
+++ FlowLayoutST/ContentView.swift
@@ -8,45 +8,69 @@
 
 import SwiftUI
 
-struct FlowLayout {
-    let spacing: UIOffset
-    let containerSize: CGSize
-    
-    init(containerSize: CGSize, spacing: UIOffset = UIOffset(horizontal: 10, vertical: 10)) {
-        self.spacing = spacing
-        self.containerSize = containerSize
-    }
-    
-    var currentX = 0 as CGFloat
-    var currentY = 0 as CGFloat
-    var lineHeight = 0 as CGFloat
-    
-    mutating func add(element size: CGSize) -> CGRect {
-        if currentX + size.width > containerSize.width {
-            currentX = 0
-            currentY += lineHeight + spacing.vertical
-            lineHeight = 0
-        }
-        defer {
-            lineHeight = max(lineHeight, size.height)
-            currentX += size.width + spacing.horizontal
-        }
-        return CGRect(origin: CGPoint(x: currentX, y: currentY), size: size)
-    }
-    
-    var size: CGSize {
-        return CGSize(width: containerSize.width, height: currentY + lineHeight)
+extension CGSize {
+    init(_ point: CGPoint) {
+        self.init(width: point.x, height: point.y)
     }
 }
 
-func flowLayout<Elements>(for elements: Elements, containerSize: CGSize, sizes: [Elements.Element.ID: CGSize]) -> [Elements.Element.ID: CGSize] where Elements: RandomAccessCollection, Elements.Element: Identifiable {
-    var state = FlowLayout(containerSize: containerSize)
-    var result: [Elements.Element.ID: CGSize] = [:]
-    for element in elements {
-        let rect = state.add(element: sizes[element.id] ?? .zero)
-        result[element.id] = CGSize(width: rect.origin.x, height: rect.origin.y)
+enum FlowAlignment {
+    case leading
+    case justify
+}
+
+func flowLayoutImpl<Id>(
+    containerSize: CGSize,
+    spacing: UIOffset = UIOffset(horizontal: 10, vertical: 10),
+    sizes: [(Id, CGSize)],
+    alignment: FlowAlignment) -> [(Id, CGRect)] {
+    var current = CGPoint.zero
+    var lineHeight = 0 as CGFloat
+
+    var result: [(Id, CGRect)] = []
+    var currentLine: [(Id, CGRect)] = []
+    
+    func startNewLine() {
+
+        let spaceLeft = containerSize.width - current.x
+        let numberOfSpaces = max(1, currentLine.count-1)
+        
+        current.x = 0
+        current.y += lineHeight + spacing.vertical
+        lineHeight = 0
+
+        let spacePerLineItem = (spaceLeft / CGFloat(numberOfSpaces)).rounded()
+ 
+        for i in currentLine.indices {
+          var (id, rect) = currentLine[i]
+            if alignment == .justify {
+              rect.origin.x += CGFloat(i) * spacePerLineItem
+          }
+          result.append((id, rect))
+        }
+        currentLine = []
     }
+    
+    for (id, size) in sizes {
+        if current.x + size.width > containerSize.width {
+          startNewLine()
+        }
+        defer {
+            lineHeight = max(lineHeight, size.height)
+            current.x += size.width + spacing.horizontal
+        }
+        currentLine.append((id, CGRect(origin: current, size: size)))
+    }
+    startNewLine()
+//    dump(result)
     return result
+}
+
+func flowLayout<Elements>(alignment: FlowAlignment = .justify) -> (_ elements: Elements, _ containerSize: CGSize, _ sizes: [Elements.Element.ID: CGSize]) -> [Elements.Element.ID: CGSize] where Elements: RandomAccessCollection, Elements.Element: Identifiable {
+    return { elements, containerSize, sizes in
+        let rects = flowLayoutImpl(containerSize: containerSize, sizes: elements.map { ($0.id, sizes[$0.id] ?? .zero) }, alignment: alignment)
+        return Dictionary(uniqueKeysWithValues: rects.map { (id, rect) in (id, CGSize(rect.origin)) })
+    }
 }
 
 
@@ -81,7 +105,7 @@ struct CollectionView<Elements, Content>: View where Elements: RandomAccessColle
         }.onPreferenceChange(CollectionViewSizeKey.self) {
             self.sizes = $0
             
-        }.background(Color.red)
+        }
     }
     
     var body: some View {
@@ -124,12 +148,12 @@ struct ContentView: View {
         VStack {
             HStack {
                 Rectangle()
-                    .fill(Color.red)
+                    .fill(Color.white)
                     .frame(width: dividerWidth)
-                CollectionView(data: strings, layout: flowLayout) {
-                    Text($0)
+                CollectionView(data: strings, layout: flowLayout(alignment: .justify)) {
+                    Text($0).foregroundColor(.white)
                         .padding(10)
-                        .background(Color.gray)
+                        .background(RoundedRectangle(cornerRadius: 5).fill(Color.blue))
                 }
             }
             Slider(value: $dividerWidth, in: 0...500)
